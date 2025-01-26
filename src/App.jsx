9979af2f3b7b808ab03996628101090b3eb17f3d
sumit sharma <sumitsharmavoice@gmail.com>
2025-01-26 13:35:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Home from "./Pages/Home";
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      color: '#1C1812',
+    },
+    fontFamily: 'Inter, sans-serif',
+    body1: {
+      fontSize: "20px",
+      fontWeight: 400
+    },
+    h1: {
+      fontSize: "56px",
+      fontWeight: 600
+    },
+    h2: {
+      fontSize: "48px",
+      fontWeight: 600
+    },
+    h4: {
+      fontSize: "32px",
+      fontWeight: 600
+    },
+    h5: {
+      fontFamily: 'Gluten, sans-serif',
+      fontSize: "20px",
+      fontWeight: 400
+    },
+    button: {
+      fontSize: "16px",
+      fontWeight: 400,
+      textTransform: "none"
+    },
+    caption: {
+      fontFamily: 'Gluten, sans-serif',
+      fontSize: "32px",
+      fontWeight: 400
+    },
+    subtitle1: {
+      fontSize: "14px",
+      fontWeight: 400
+    },
+    subtitle2: {
+      fontSize: "12px",
+      fontWeight: 400
+    }
+  },
+  palette: {
+    primary: {
+      main: '#1C1812',
+    },
+    secondary: {
+      main: '#AEADAA',
+    },
+    info: {
+      main: "#65625E"
+    }
+  },
+});
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  )
+}
+
+export default App
